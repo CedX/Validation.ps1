528cba75a866621145986d8223cb735836337074
Cédric Belin <cedx@outlook.com>
2026-06-27 15:52:19 +0200

    Add an implicit conversion

--- Sources/Validator.cs
+++ Sources/Validator.cs
@@ -1,5 +1,6 @@
 namespace Belin.Validation;
 
+using System.Collections;
 using System.Globalization;
 using System.Management.Automation;
 
@@ -24,4 +25,17 @@ public class Validator {
 	/// <param name="value">The value to be validated.</param>
 	/// <returns><see langword="true"/> if the specified value is valid otherwise <see langword="false"/>.</returns>
 	public bool IsValid(object? value) => Convert.ToBoolean(Test.Invoke(value).Last().BaseObject, CultureInfo.InvariantCulture);
+
+	/// <summary>
+	/// Creates a new validator from the specified hash table.
+	/// </summary>
+	/// <param name="hashtable">The hash table providing the validator properties.</param>
+	/// <returns>The validator corresponding to the specified hash table.</returns>
+	/// <exception cref="ArgumentException">TODO</exception>
+	public static implicit operator Validator(Hashtable hashtable) {
+		return new Validator {
+			Reason = hashtable["Reason"] is string reason ? reason : throw new ArgumentException("TODO", nameof(hashtable)),
+			Test = hashtable["Test"] is ScriptBlock test ? test : throw new ArgumentException("TODO", nameof(hashtable))
+		};
+	}
 }
