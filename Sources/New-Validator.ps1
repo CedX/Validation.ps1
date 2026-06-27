55ad626af7b4b36d8b48769b5f0b4d92868d05f5
Cédric Belin <cedx@outlook.com>
2026-06-27 15:42:29 +0200

    Add the `New-Validator` cmdlet

---
+++ Sources/New-Validator.ps1
@@ -0,0 +1,26 @@
+﻿using namespace Belin.Validation
+
+<#
+.SYNOPSIS
+	Creates a new validator.
+.OUTPUTS
+	The newly created validator.
+#>
+function New-Validator {
+	[CmdletBinding()]
+	[OutputType([Belin.Validation.Validator])]
+	param (
+		# The error message describing the validation failure.
+		[Parameter(Mandatory, Position = 0)]
+		[string] $Reason,
+
+		# The script block used to perform the validation.
+		[Parameter(Mandatory, Position = 1)]
+		[scriptblock] $Test
+	)
+
+	[Validator]@{
+		Reason = $Reason
+		Test = $Test
+	}
+}
