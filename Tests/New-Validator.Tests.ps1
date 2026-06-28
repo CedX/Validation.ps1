04617a191544e61769c96cc13679541535786526
Cédric Belin <cedx@outlook.com>
2026-06-28 15:09:22 +0200

    Add tests for the `New-Validator` cmdlet

--- Tests/New-Validator.Tests.ps1
+++ Tests/New-Validator.Tests.ps1
@@ -1,4 +1,5 @@
-﻿using module ../Validation.psd1
+﻿using namespace System.Diagnostics.CodeAnalysis
+using module ../Validation.psd1
 
 <#
 .SYNOPSIS
@@ -7,9 +8,57 @@
 Describe "New-Validator" {
 	Context "ImplicitConversion" {
 		It "should create a validator from the specified hash table" {
-			[Belin.Validation.Validator] $validator = @{ Reason = "An error occurred."; Test = {} }
+			[Belin.Validation.Validator] $validator = @{ Reason = "An error occurred."; Test = { $false } }
 			$validator.Reason | Should -BeExactly "An error occurred."
 			$validator.Test | Should -BeOfType ([scriptblock])
+		}
+
+		It "should throw an exception when the hash table does not provide the expected properties" -ForEach @(
+			@{ Reason = "An error occurred." }
+			@{ Test = { $false } }
+			@{ Reason = 123; Test = { $false } }
+			@{ Reason = "An error occurred."; Test = "false" }
+		) {
+			$scriptBlock = {
+				[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "validator")]
+				[Belin.Validation.Validator] $validator = $_
+			}
+
+			$scriptBlock | Should -Throw
+		}
+	}
+
+	Context "IsValid" {
+		It "should return `$false if the validated value is invalid" -ForEach @(
+			@{ Value = $false; Test = { $_ } }
+			@{ Value = 123; Test = { $_ -eq 456 } }
+			@{ Value = "foo"; Test = { $_ -ceq "FOO" } }
+			@{ Value = @{ Name = "Cédric" }; Test = { $_.Name -eq "Belin" } }
+		) {
+			$validator = New-ValidationValidator "An error occurred." $test
+			$validator.IsValid($value) | Should -BeFalse
+		}
+
+		It "should return `$true if the validated value is valid" -ForEach @(
+			@{ Value = $true; Test = { $_ } }
+			@{ Value = 123; Test = { $_ -eq 123 } }
+			@{ Value = "foo"; Test = { $_ -eq "FOO" } }
+			@{ Value = @{ Name = "Cédric" }; Test = { $_.Name -eq "Cédric" } }
+		) {
+			$validator = New-ValidationValidator "An error occurred." $test
+			$validator.IsValid($value) | Should -BeTrue
+		}
+	}
+
+	Context "Reason" {
+		It "should be editable from the validation script block" {
+			$validator = New-ValidationValidator "An error occurred." {
+				$this.Reason = "The error message has been updated."
+				return $false
+			}
+
+			$validator.IsValid(123)
+			$validator.Reason | Should -BeExactly "The error message has been updated."
 		}
 	}
 }
