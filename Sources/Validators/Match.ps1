45fef051e53bffefde621a0cf68e82ec2a4450f4
Cédric Belin <cedx@outlook.com>
2026-07-03 23:46:16 +0200

    Add the `New-ValidatorMatch` and `New-ValidatorNotMatch` cmdlets

---
+++ Sources/Validators/Match.ps1
@@ -0,0 +1,59 @@
+﻿using namespace Belin.Validation
+
+<#
+.SYNOPSIS
+	Creates a new validator that ensures the validated value matches a given regex pattern.
+.OUTPUTS
+	The newly created validator.
+#>
+function New-ValidatorMatch {
+	[CmdletBinding()]
+	[OutputType([Belin.Validation.Validator])]
+	param (
+		# The pattern to match.
+		[Parameter(Mandatory, Position = 0)]
+		[string] $Pattern,
+
+		# The error message describing the validation failure.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Reason,
+
+		# Value indicating whether to perform a case-sensitive comparison.
+		[switch] $CaseSensitive
+	)
+
+	return [ComparisonValidator[string]]@{
+		Reason = $Reason
+		Test = $CaseSensitive ? { $_ -cmatch $this.Value } : { $_ -imatch $this.Value }
+		Value = $Pattern
+	}
+}
+
+<#
+.SYNOPSIS
+	Creates a new validator that ensures the validated value does not match a given regex pattern.
+.OUTPUTS
+	The newly created validator.
+#>
+function New-ValidatorNotMatch {
+	[CmdletBinding()]
+	[OutputType([Belin.Validation.Validator])]
+	param (
+		# The pattern to match.
+		[Parameter(Mandatory, Position = 0)]
+		[string] $Pattern,
+
+		# The error message describing the validation failure.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Reason,
+
+		# Value indicating whether to perform a case-sensitive comparison.
+		[switch] $CaseSensitive
+	)
+
+	return [ComparisonValidator[string]]@{
+		Reason = $Reason
+		Test = $CaseSensitive ? { $_ -cnotmatch $this.Value } : { $_ -inotmatch $this.Value }
+		Value = $Pattern
+	}
+}
---
+++ Tests/Validators/Match.Tests.ps1
@@ -0,0 +1,49 @@
+﻿using module ../../Validation.psd1
+
+<#
+.SYNOPSIS
+	Tests the features of the `New-ValidatorMatch` cmdlet.
+#>
+Describe "New-ValidatorMatch" {
+	It "should return `$true if the value matches the specified pattern" -ForEach @(
+		@{ Value = ""; Pattern = ".*" }
+		@{ Value = "foo"; Pattern = "^FOO$" }
+		@{ Value = "bar"; Pattern = "^bar?" }
+		@{ Value = "baz"; Pattern = ".z$" }
+	) {
+		(New-ValidatorMatch $pattern "Reason").IsValid($value) | Should -BeTrue
+	}
+
+	It "should return `$false if the value does not match the specified pattern" -ForEach @(
+		@{ Value = ""; Pattern = "." }
+		@{ Value = "foo"; Pattern = "bar" }
+		@{ Value = "baz"; Pattern = "ar$" }
+		@{ Value = "qux"; Pattern = "^qi" }
+	) {
+		(New-ValidatorMatch $pattern "Reason").IsValid($value) | Should -BeFalse
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of the `New-ValidatorNotMatch` cmdlet.
+#>
+Describe "New-ValidatorNotMatch" {
+	It "should return `$true if the value does not match the specified pattern" -ForEach @(
+		@{ Value = ""; Pattern = "." }
+		@{ Value = "foo"; Pattern = "bar" }
+		@{ Value = "baz"; Pattern = "ar$" }
+		@{ Value = "qux"; Pattern = "^qi" }
+	) {
+		(New-ValidatorNotMatch $pattern "Reason").IsValid($value) | Should -BeTrue
+	}
+
+	It "should return `$false if the value matches the specified pattern" -ForEach @(
+		@{ Value = ""; Pattern = ".*" }
+		@{ Value = "foo"; Pattern = "^FOO$" }
+		@{ Value = "bar"; Pattern = "^bar?" }
+		@{ Value = "baz"; Pattern = ".z$" }
+	) {
+		(New-ValidatorNotMatch $pattern "Reason").IsValid($value) | Should -BeFalse
+	}
+}
