2f57bbf51db9f350b48b579170260d7de47fff9f
Cédric Belin <cedx@outlook.com>
2026-07-04 21:49:34 +0200

    Add the `New-ValidatorIn` and `New-ValidatorNotIn` cmdlets

---
+++ Sources/Validators/In.ps1
@@ -0,0 +1,59 @@
+﻿using namespace Belin.Validation
+
+<#
+.SYNOPSIS
+	Creates a new validator that ensures the validated value matches an element from a set of possible values.
+.OUTPUTS
+	The newly created validator.
+#>
+function New-ValidatorIn {
+	[CmdletBinding()]
+	[OutputType([Belin.Validation.Validator])]
+	param (
+		# The set of possible values.
+		[Parameter(Mandatory, Position = 0)]
+		[object[]] $Values,
+
+		# The error message describing the validation failure.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Reason,
+
+		# Value indicating whether to perform a case-sensitive comparison.
+		[switch] $CaseSensitive
+	)
+
+	return [ComparisonValidator[object[]]]@{
+		Reason = $Reason
+		Test = $CaseSensitive ? { $_ -cin $this.Value } : { $_ -iin $this.Value }
+		Value = $Values
+	}
+}
+
+<#
+.SYNOPSIS
+	Creates a new validator that ensures the validated value does not match an element from a set of possible values.
+.OUTPUTS
+	The newly created validator.
+#>
+function New-ValidatorNotIn {
+	[CmdletBinding()]
+	[OutputType([Belin.Validation.Validator])]
+	param (
+		# The set of possible values.
+		[Parameter(Mandatory, Position = 0)]
+		[object[]] $Values,
+
+		# The error message describing the validation failure.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Reason,
+
+		# Value indicating whether to perform a case-sensitive comparison.
+		[switch] $CaseSensitive
+	)
+
+	return [ComparisonValidator[object[]]]@{
+		Reason = $Reason
+		Test = $CaseSensitive ? { $_ -cnotin $this.Value } : { $_ -inotin $this.Value }
+		Value = $Values
+	}
+}
---
+++ Tests/Validators/In.Tests.ps1
@@ -0,0 +1,41 @@
+﻿using module ../../Validation.psd1
+
+<#
+.SYNOPSIS
+	Tests the features of the `New-ValidatorIn` cmdlet.
+#>
+Describe "New-ValidatorIn" {
+	It "should return `$true if the property matches an element from a set of values" -ForEach @(
+		@{ Property = "FOO"; Values = "foo", "bar", "baz" }
+		@{ Property = 123; Values = 123, 456, 789 }
+	) {
+		(New-ValidatorIn $values "Reason").IsValid($property) | Should -BeTrue
+	}
+
+	It "should return `$false if the property does not match an element from a set of values" -ForEach @(
+		@{ Property = "ABC"; Values = "AB", "CD", "EF" }
+		@{ Property = 666; Values = 123, 456, 789 }
+	) {
+		(New-ValidatorIn $values "Reason").IsValid($property) | Should -BeFalse
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of the `New-ValidatorNotIn` cmdlet.
+#>
+Describe "New-ValidatorNotIn" {
+	It "should return `$true if the property does not match an element from a set of values" -ForEach @(
+		@{ Property = "ABC"; Values = "AB", "CD", "EF" }
+		@{ Property = 666; Values = 123, 456, 789 }
+	) {
+		(New-ValidatorNotIn $values "Reason").IsValid($property) | Should -BeTrue
+	}
+
+	It "should return `$false if the property matches an element from a set of values" -ForEach @(
+		@{ Property = "FOO"; Values = "foo", "bar", "baz" }
+		@{ Property = 123; Values = 123, 456, 789 }
+	) {
+		(New-ValidatorNotIn $values "Reason").IsValid($property) | Should -BeFalse
+	}
+}
