355e31025803323bc3fa95085c67ccb53a8ee7a3
Cédric Belin <cedx@outlook.com>
2026-07-26 18:04:26 +0200

    Fix the tests

--- Tests/Assert-Validation.Tests.ps1
+++ Tests/Assert-Validation.Tests.ps1
@@ -3,9 +3,9 @@ using module ../Validation.psd1
 
 <#
 .SYNOPSIS
-	Tests the features of the `Test-Validation` cmdlet.
+	Tests the features of the `Assert-Validation` cmdlet.
 #>
-Describe "Test-Validation" {
+Describe "Assert-Validation" {
 	BeforeAll {
 		[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "hashtable")]
 		$hashtable = @{ FirstName = "Cédric"; Gender = "Male" }
@@ -16,24 +16,32 @@ Describe "Test-Validation" {
 
 	It "should return an empty hash table if there are no validation errors" {
 		foreach ($instance in $hashtable, $object) {
-			Should-BeTrue (Test-Validation $instance @{ FirstName = New-ValidatorNotEmpty "The first name is required." })
+			$errors = Assert-Validation $instance @{ FirstName = New-ValidatorNotEmpty "The first name is required." }
+			Should-Be 0 $errors.Count
 		}
 	}
 
 	It "should return a non-empty hash table if there are validation errors" {
 		foreach ($instance in $hashtable, $object) {
-			Should-BeFalse (Test-Validation $instance @{ LastName = New-ValidatorNotEmpty "The last name is required." })
+			$errors = Assert-Validation $instance @{ LastName = New-ValidatorNotEmpty "The last name is required." }
+			Should-Be 1 $errors.Count
+			Should-BeString "The last name is required." $errors.LastName -CaseSensitive
 		}
 	}
 
 	It "should support multiple validators per property" {
 		foreach ($instance in $hashtable, $object) {
-			Should-BeFalse (Test-Validation $instance @{
+			$errors = Assert-Validation $instance @{
 				FirstName = (New-ValidatorNotEmpty "The first name is required."), (New-ValidatorLike "C*" "The first name must start with the letter C.")
 				Gender = (New-ValidatorNotEmpty "The gender is empty."), (New-ValidatorEqual "Female" "Only women are allowed.")
 				LastName = New-ValidatorNotEmpty "The last name is required."
 				Password = (New-ValidatorNotEmpty "The password is empty."), (New-ValidatorMinLength 5 "The password is too short.")
-			})
+			}
+
+			Should-Be 3 $errors.Count
+			Should-BeString "Only women are allowed." $errors.Gender -CaseSensitive
+			Should-BeString "The last name is required." $errors.LastName -CaseSensitive
+			Should-BeString "The password is empty." $errors.Password -CaseSensitive
 		}
 	}
 }
