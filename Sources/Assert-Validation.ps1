75c492ab88345b5c9b503d7a580a6a7f4f1e21df
Cédric Belin <cedx@outlook.com>
2026-07-26 17:54:54 +0200

    Rename the `Test-Validation` cmdlet to `Assert-Validation`

---
+++ Sources/Assert-Validation.ps1
@@ -0,0 +1,39 @@
+﻿using namespace Belin.Validation
+
+<#
+.SYNOPSIS
+	Performs the data validation on the specified object according to a given set of validation rules.
+.INPUTS
+	The object to validate.
+.OUTPUTS
+	The validation errors, if any.
+#>
+function Assert-Validation {
+	[CmdletBinding()]
+	[OutputType([hashtable])]
+	param (
+		# The object to validate.
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[object] $Object,
+
+		# The set of validation rules to apply.
+		[Parameter(Mandatory, Position = 2)]
+		[hashtable] $RuleSet
+	)
+
+	process {
+		$errors = @{}
+
+		foreach ($property in $RuleSet.Keys) {
+			foreach ($rule in @($RuleSet[$property])) {
+				$validator = [Validator] $rule
+				if (-not $validator.IsValid($Object.$property)) {
+					$errors[$property] = $validator.Reason
+					break
+				}
+			}
+		}
+
+		$errors
+	}
+}
