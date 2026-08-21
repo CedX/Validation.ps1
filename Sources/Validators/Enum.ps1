e36154004f29e19b8e7d8e797ec113c37a715a1a
Cédric Belin <cedx@outlook.com>
2026-08-21 09:11:49 +0200

    Add the `New-ValidatorEnum` cmdlet

---
+++ Sources/Validators/Enum.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+	Creates a new validator that ensures the validated value exists in a specified enumeration.
+.OUTPUTS
+	The newly created validator.
+#>
+function New-ValidatorEnum {
+	[CmdletBinding()]
+	[OutputType([Belin.Validation.Validator])]
+	param (
+		# An enumeration type.
+		[Parameter(Mandatory, Position = 1)]
+		[Type] $Type,
+
+		# The error message describing the validation failure.
+		[Parameter(Mandatory, Position = 2)]
+		[string] $Reason
+	)
+
+	New-Validator $Reason { [Enum]::IsDefined($Type, $_) }
+}
