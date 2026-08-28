4e227ee806f25dd088d669342692e124f8bc1e65
Cédric Belin <cedx@outlook.com>
2026-08-29 01:44:26 +0200

    Module merging

--- Sources/Validation.ps1
+++ Sources/Validation.ps1
@@ -37,3 +37,36 @@ function Assert-Validation {
 		$errors
 	}
 }
+
+<#
+.SYNOPSIS
+	Performs the data validation on the specified object according to a given set of validation rules.
+.INPUTS
+	The object to validate.
+.OUTPUTS
+	`$true` if the validated object is valid, otherwise `$false`.
+#>
+function Test-Validation {
+	[CmdletBinding()]
+	[OutputType([bool])]
+	param (
+		# The object to validate.
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[object] $InputObject,
+
+		# The set of validation rules to apply.
+		[Parameter(Mandatory, Position = 2)]
+		[hashtable] $RuleSet
+	)
+
+	process {
+		foreach ($property in $RuleSet.Keys) {
+			foreach ($rule in @($RuleSet[$property])) {
+				$validator = [Validator] $rule
+				if (-not $validator.IsValid($InputObject.$property)) { return $false }
+			}
+		}
+
+		$true
+	}
+}
--- Tests/Validation.Tests.ps1
+++ Tests/Validation.Tests.ps1
@@ -45,3 +45,40 @@ Describe "Assert-Validation" {
 		}
 	}
 }
+
+<#
+.SYNOPSIS
+	Tests the features of the `Test-Validation` cmdlet.
+#>
+Describe "Test-Validation" {
+	BeforeAll {
+		[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "hashtable")]
+		$hashtable = @{ FirstName = "Cédric"; Gender = "Male" }
+
+		[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "object")]
+		$psObject = [pscustomobject] $hashtable
+	}
+
+	It "should return `$true if there are no validation errors" {
+		foreach ($object in $hashtable, $psObject) {
+			Should-BeTrue (Test-Validation $object @{ FirstName = New-ValidatorNotEmpty "The first name is required." })
+		}
+	}
+
+	It "should return `$false if there are validation errors" {
+		foreach ($object in $hashtable, $psObject) {
+			Should-BeFalse (Test-Validation $object @{ LastName = New-ValidatorNotEmpty "The last name is required." })
+		}
+	}
+
+	It "should support multiple validators per property" {
+		foreach ($object in $hashtable, $psObject) {
+			Should-BeFalse (Test-Validation $object @{
+				FirstName = (New-ValidatorNotEmpty "The first name is required."), (New-ValidatorLike "C*" "The first name must start with the letter C.")
+				Gender = (New-ValidatorNotEmpty "The gender is empty."), (New-ValidatorEqual "Female" "Only women are allowed.")
+				LastName = New-ValidatorNotEmpty "The last name is required."
+				Password = (New-ValidatorNotEmpty "The password is empty."), (New-ValidatorMinLength 5 "The password is too short.")
+			})
+		}
+	}
+}
